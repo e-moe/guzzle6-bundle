8edf8a0b6a4fb9a3ff497225f653984fbee7a2be
Nikolay Labinskiy <e-moe@ukr.net>
2016-01-01 12:10:48 +0200

    log atapter tests

---
+++ src/Tests/Log/ArrayLogAdapterTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Doctrine\Bundle\DoctrineBundle\Tests\Log;
+
+use Emoe\GuzzleBundle\Log\ArrayLogAdapter;
+
+class ArrayLogAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  ArrayLogAdapter */
+    protected $logAdapter;
+
+    public function setUp()
+    {
+        $this->logAdapter = new ArrayLogAdapter();
+    }
+
+    /**
+     * @dataProvider logsProvider
+     */
+    public function testLogs(array $input, array $expected)
+    {
+        foreach ($input as $data) {
+            call_user_func_array([$this->logAdapter, 'log'], $data);
+        }
+        $this->assertSame($expected, $this->logAdapter->getLogs());
+    }
+
+    /**
+     * @dataProvider logsProvider
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function testClearLogs(array $input, array $expected)
+    {
+        $this->assertCount(0, $this->logAdapter->getLogs());
+        foreach ($input as $data) {
+            call_user_func_array([$this->logAdapter, 'log'], $data);
+        }
+        $this->assertCount(count($input), $this->logAdapter->getLogs());
+        $this->logAdapter->clearLogs();
+        $this->assertCount(0, $this->logAdapter->getLogs());
+    }
+
+    public function logsProvider()
+    {
+        return [
+            [
+                [],
+                [],
+            ],
+            [
+                [
+                    ['test'],
+                ],
+                [
+                    [
+                        'message' => 'test',
+                        'priority' => LOG_INFO,
+                        'extras' => [],
+                    ],
+                ],
+            ],
+            [
+                [
+                    ['test 1', LOG_DEBUG],
+                    ['test 2', LOG_ERR, ['extra' => 'data']],
+                ],
+                [
+                    [
+                        'message' => 'test 1',
+                        'priority' => LOG_DEBUG,
+                        'extras' => [],
+                    ],
+                    [
+                        'message' => 'test 2',
+                        'priority' => LOG_ERR,
+                        'extras' => ['extra' => 'data'],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
---
+++ src/Tests/Log/MonologLogAdapterTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Doctrine\Bundle\DoctrineBundle\Tests\Log;
+
+use Emoe\GuzzleBundle\Log\MonologLogAdapter;
+use Monolog\Logger;
+
+class MonologLogAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var  MonologLogAdapter */
+    protected $logAdapter;
+
+    protected $logger;
+
+    public function setUp()
+    {
+        $this->logger = $this->getMockBuilder('Monolog\Logger')->disableOriginalConstructor()->getMock();
+        $this->logAdapter = new MonologLogAdapter($this->logger);
+    }
+
+    public function testGetLogObject()
+    {
+        $this->assertSame($this->logger, $this->logAdapter->getLogObject());
+    }
+
+    /**
+     * @dataProvider logsProvider
+     */
+    public function testLogs(array $input)
+    {
+        $this->logger->expects($this->exactly(count($input)))->method('addRecord');
+        foreach ($input as $data) {
+            call_user_func_array([$this->logAdapter, 'log'], $data);
+        }
+    }
+
+    /**
+     * @dataProvider mapProvider
+     */
+    public function testMapping($input, $expected)
+    {
+        $this->logger->expects($this->once())
+            ->method('addRecord')
+            ->with(
+                $expected,
+                'test'
+            );
+        $this->logAdapter->log('test', $input);
+    }
+
+    public function logsProvider()
+    {
+        return [
+            [
+                [],
+            ],
+            [
+                [
+                    ['test'],
+                ],
+            ],
+            [
+                [
+                    ['test 1', LOG_DEBUG],
+                    ['test 2', LOG_ERR, ['extra' => 'data']],
+                ],
+            ],
+        ];
+    }
+
+    public function mapProvider()
+    {
+        return [
+            [LOG_DEBUG, Logger::DEBUG],
+            [LOG_INFO, Logger::INFO],
+            [LOG_WARNING, Logger::WARNING],
+            [LOG_ERR, Logger::ERROR],
+            [LOG_CRIT, Logger::CRITICAL],
+            [LOG_ALERT, Logger::ALERT],
+        ];
+    }
+}
